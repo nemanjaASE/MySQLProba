55223bbde500bb4ef5676aeb275e2409720838e8
nemanjaASE <nightwalker44ASE@gmail.com>
2022-05-18 17:05:40 +0200

    Error added

--- 123/main.py
+++ 123/main.py
@@ -1,17 +1,18 @@
 from mysql.connector import connect, Error
 
 if __name__ == '__main__':
-    with connect(
-        host='127.0.0.1',
-        user='admin',
-        password='admin',
-        database='RES_PROJEKAT'
-    ) as connecting:
-        query = "SELECT * FROM potrosnjaBrojila pb, brojilo b where pb.IdBrojila = b.IdBrojila;"
-        with connecting.cursor() as cursor:
-            cursor.execute(query)
-            result = cursor.fetchall()
-            for res in result:
-                print(res)
-
-
+    try:
+        with connect(
+                host='127.0.0.1',
+                user='admin',
+                password='admin',
+                database='RES_PROJEKAT'
+        ) as connecting:
+            query = "SELECT * FROM potrosnjaBrojila pb, brojilo b where pb.IdBrojila = b.IdBrojila;"
+            with connecting.cursor() as cursor:
+                cursor.execute(query)
+                result = cursor.fetchall()
+                for res in result:
+                    print(res)
+    except Error as e:
+        print(e)
